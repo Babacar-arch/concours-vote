935849e49fb486d1bcc789c3df6471ecb306788b
Babacar Ndong <ndongbabacar100@gmail.com>
2021-11-17 12:05:11 +0000

    Deploy with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'concoursVote';
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDNFkPKSiZ4vgmZLW_2rUbeBzUeoj1Ecyk",
+      authDomain: "concours-vote.firebaseapp.com",
+      projectId: "concours-vote",
+      storageBucket: "concours-vote.appspot.com",
+      messagingSenderId: "941192646293",
+      appId: "1:941192646293:web:c57a0186968bb8aff0159c",
+      measurementId: "G-PQJGRTZC8L"
+    };
+
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+  }
 }
