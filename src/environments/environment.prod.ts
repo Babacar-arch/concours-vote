d472d23807d7d165379bdc35ea9482765d50f428
Babacar Ndong <ndongbabacar100@gmail.com>
2021-11-17 12:28:49 +0000

    Adding the Firebase config in envi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDNFkPKSiZ4vgmZLW_2rUbeBzUeoj1Ecyk",
+    authDomain: "concours-vote.firebaseapp.com",
+    databaseURL: "https://concours-vote-default-rtdb.firebaseio.com",
+    projectId: "concours-vote",
+    storageBucket: "concours-vote.appspot.com",
+    messagingSenderId: "941192646293",
+    appId: "1:941192646293:web:c57a0186968bb8aff0159c",
+    measurementId: "G-PQJGRTZC8L"
+  }
 };
